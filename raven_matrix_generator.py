7a9d105f81d5c388d58e3d2a5308e2a77a4c8359
qwer239 <67898589+qwer239@users.noreply.github.com>
2025-04-27 20:15:27 +0900

    Update raven_matrix_generator.py

--- raven_matrix_generator.py
+++ raven_matrix_generator.py
@@ -1,8 +1,5 @@
-import numpy as np
 from PIL import Image, ImageDraw
 import random
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4
 
 class RavenMatrixGenerator:
     def __init__(self, type_num=None, difficulty=3, cell_size=150, grid_size=3):
@@ -13,44 +10,41 @@ class RavenMatrixGenerator:
         self.margin = 10
         self.W = cell_size * grid_size + self.margin * (grid_size + 1)
         self.H = cell_size * grid_size + self.margin * (grid_size + 1) + cell_size + 3 * self.margin
-
-        # 문제 10번(선분 결합 패턴)만 임시로 활성화
-        self.type_generators = {
-            10: self.generate_line_combination
-        }
+        
+        # 문제 10번만 활성화
+        self.type_generators = {10: self.generate_line_combination}
 
     def generate_problem(self, type_num=None):
-        if type_num is None:
-            type_num = 10
-        return self.type_generators[type_num]()
+        return self.generate_line_combination()
 
     def generate_line_combination(self):
         img = Image.new('RGB', (self.W, self.H), 'white')
         draw = ImageDraw.Draw(img)
-        patterns = [
-            [[(0.2, 0.2, 0.8, 0.8)], [(0.2, 0.2, 0.8, 0.2)], [(0.8, 0.2, 0.8, 0.8)]],
-            [[(0.2, 0.8, 0.8, 0.2)], [(0.2, 0.8, 0.8, 0.8)], [(0.2, 0.2, 0.2, 0.8)]],
-            [[(0.2, 0.8, 0.8, 0.2)], [(0.2, 0.2, 0.8, 0.8)], None]
-        ]
+        
+        # 3x3 매트릭스 그리기
         for i in range(3):
             for j in range(3):
                 x = self.margin + j * (self.cell_size + self.margin)
                 y = self.margin + i * (self.cell_size + self.margin)
+                draw.rectangle((x, y, x+self.cell_size, y+self.cell_size), outline='black')
+                
                 if i == 2 and j == 2:
-                    draw.rectangle((x, y, x+self.cell_size, y+self.cell_size), outline='black')
-                    draw.text((x+self.cell_size//2-10, y+self.cell_size//2-10), '?', fill='black')
+                    draw.text((x+self.cell_size//2-5, y+self.cell_size//2-10), '?', fill='black')
                 else:
-                    self._draw_lines(draw, (x, y), patterns[i][j])
+                    if i == 0 and j == 0:
+                        draw.line((x+20, y+20, x+self.cell_size-20, y+self.cell_size-20), fill='black', width=3)
+                    elif i == 0 and j == 1:
+                        draw.line((x+20, y+20, x+self.cell_size-20, y+20), fill='black', width=3)
+                    elif i == 0 and j == 2:
+                        draw.line((x+self.cell_size-20, y+20, x+self.cell_size-20, y+self.cell_size-20), fill='black', width=3)
+        
+        # 보기(A-E) 추가
+        for idx in range(5):
+            x = self.margin + idx * (self.cell_size + self.margin)
+            y = self.margin*(self.grid_size+1) + self.cell_size*self.grid_size
+            draw.rectangle((x, y, x+self.cell_size, y+self.cell_size), outline='black')
+            draw.text((x+self.cell_size//2-5, y+self.cell_size//2-10), chr(65+idx), fill='black')
+        
         return img, 'A'
 
-    def _draw_lines(self, draw, pos, lines):
-        x, y = pos
-        for line in lines:
-            x1, y1, x2, y2 = line
-            draw.line((
-                x + x1*self.cell_size, 
-                y + y1*self.cell_size,
-                x + x2*self.cell_size, 
-                y + y2*self.cell_size
-            ), fill='black', width=3)
 
